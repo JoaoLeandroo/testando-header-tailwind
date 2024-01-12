15e4da104c4e78dde04e38bb5b388c78e0914535
joaoleandro <new.joaoleandro@gmail.com>
2024-01-11 22:01:24 -0300

    menu responsivo, testes

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,6 @@ export default function Home() {
   return (
     <Container>
       <div className="w-full bg-yellow-500 mt-12">
-        {/* <div className="w-full h-[250px] bg-green-200"></div>
-        <div className="w-full h-[250px] bg-green-300"></div>
-        <div className="w-full h-[250px] bg-green-400"></div>
-        <div className="w-full h-[250px] bg-green-500"></div>
-        <div className="w-full h-[250px] bg-green-600"></div>
-        <div className="w-full h-[250px] bg-green-700"></div>         */}
       </div>
     </Container>
   )
--- components/Nav-bar.tsx
+++ components/Nav-bar.tsx
@@ -1,33 +1,83 @@
+'use client'
+
+import { useState } from "react";
 import Container from "./Container";
 import Link from "next/link";
+import Image from "next/image";
 
 const NavBar = () => {
+
+  const [open, setOpen] = useState(true)
+
+  function menu() {
+    if(open === false) return setOpen(true)
+
+    setOpen(false)
+  }
+
   return (
-    <header className="fixed w-full top-0">
+    <header className="fixed w-full top-0 border-b border-slate-700">
       <Container>
-        <div className="flex justify-around h-full">
+        <div className="flex sm:justify-around h-full justify-between">
           <div>
             <Link className="font-semibold text-2xl" href={'/'} title="TheForce">TheForce</Link>
           </div>
 
-          <nav>
-            <ul className="flex gap-3 h-full items-center flex-col sm:flex-row sm:bg-black">
-              <li>
-                <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>Home</Link>
-              </li>
-              <li>
-                <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>About</Link>
-              </li>
-              <li>
-                <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>Donate</Link>
-              </li>
-              <li>
-                <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>Account</Link>
-              </li>
-              <li>
-                <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>Contact</Link>
-              </li>
-            </ul>
+          <nav className="flex items-center">
+            {
+              open === true ?
+              <div>
+                <Image
+                  onClick={menu}
+                  src={'/lista.png'}
+                  width={35}
+                  height={35}
+                  alt="menu"
+                  className="cursor-pointer block sm:hidden"
+                /> 
+
+                <ul className="sm:flex gap-3 h-full items-center flex-col sm:flex-row hidden">
+                  <li>
+                    <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>Home</Link>
+                  </li>
+                  <li>
+                    <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>About</Link>
+                  </li>
+                  <li>
+                    <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>Donate</Link>
+                  </li>
+                  <li>
+                    <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>Account</Link>
+                  </li>
+                  <li>
+                    <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>Contact</Link>
+                  </li>
+                </ul>
+
+              </div>
+                
+                :
+                <ul className="sm:flex gap-3 h-full items-center flex-col sm:flex-row">
+                  <button onClick={menu} className="text-white text-5xl font-bold relative left-2">x</button>
+                  <li>
+                    <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>Home</Link>
+                  </li>
+                  <li>
+                    <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>About</Link>
+                  </li>
+                  <li>
+                    <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>Donate</Link>
+                  </li>
+                  <li>
+                    <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>Account</Link>
+                  </li>
+                  <li>
+                    <Link className="hover:text-slate-500 hover:underline transition duration-500 underline-offset-4" href={"/"}>Contact</Link>
+                  </li>
+                </ul>
+            }
+
+
           </nav>
         </div>
       </Container>
